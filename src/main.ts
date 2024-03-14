b4846bfccee229e46143e84875c05f2846d087bc
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-13 21:15:05 -0400

    got it working

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,35 @@
-const endpoint = 'https://webserver-production-e620.up.railway.app/'
+const endpoint = import.meta.env.VITE_APIURL
 
 async function getData(url: string) {
   try {
-    const response = await fetch(url)
+    const response = await fetch(url);
     if (!response.ok) {
-      throw new Error (`${response.status}: ${response.statusText}`)
+      throw new Error(`${response.status}: ${response.statusText}`);
     }
-    const data = response.json()
-    console.log(data)
+    const data = await response.json();
+    return data
   } catch (error) {
-    console.error('There was an unexpected error =', error)
+    console.error("There was an unexpected error =", error);
   }
-
-  // const appData = document.querySelector('#app')
-  // appData!.innerHTML = `
-  //   <h1>Monthly Budget<h1>
-  //   <p>${data}</p>
-  // `
 }
 
-getData(endpoint)
+async function renderData(url: string) {
+  const data = await getData(url);
+  for (let i = 0; i < data.length; i++) {
+    const start = document.querySelector('#start')
+    const row = document.createElement('tr')
+    row.id = i.toString();
+    start?.appendChild(row)
+    const category = document.createElement('td')
+    const description = document.createElement('td')
+    const amount = document.createElement('td')
+    category.textContent = data[i].category
+    description.textContent = data[i].description
+    amount.textContent = data[i].amount
+    row?.appendChild(category)
+    row?.appendChild(description)
+    row?.appendChild(amount)
+  }
+}
+
+renderData(endpoint);
