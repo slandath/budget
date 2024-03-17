26e2242c369bd2f86b82cbb3ce4b5db1cabf5700
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-17 15:01:42 -0400

    added form submission

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
-const getEndpoint = import.meta.env.VITE_GETAPIURL
+const getEndpoint = import.meta.env.VITE_GETAPIURL;
 const postEndpoint = import.meta.env.VITE_POSTAPIURL
-const form = document.querySelector('form')!;
 
-// Event Listeners
-form?.addEventListener("submit", function(e) {
-  formSubmit(e,postEndpoint)
-})
-
+const formEl: HTMLFormElement = document.querySelector("#form")!;
 
 async function getData(url: string) {
   try {
@@ -15,7 +10,7 @@ async function getData(url: string) {
       throw new Error(`${response.status}: ${response.statusText}`);
     }
     const data = await response.json();
-    return data
+    return data;
   } catch (error) {
     console.error("There was an unexpected error =", error);
   }
@@ -24,33 +19,33 @@ async function getData(url: string) {
 async function renderData(url: string) {
   const data = await getData(url);
   for (let i = 0; i < data.length; i++) {
-    const start = document.querySelector('#start')
-    const row = document.createElement('tr')
+    const start = document.querySelector("#start");
+    const row = document.createElement("tr");
     row.id = i.toString();
-    start?.appendChild(row)
-    const category = document.createElement('td')
-    const description = document.createElement('td')
-    const amount = document.createElement('td')
-    category.textContent = data[i].category
-    description.textContent = data[i].description
-    amount.textContent = data[i].amount
-    row?.appendChild(category)
-    row?.appendChild(description)
-    row?.appendChild(amount)
+    start?.appendChild(row);
+    const category = document.createElement("td");
+    const description = document.createElement("td");
+    const amount = document.createElement("td");
+    category.textContent = data[i].category;
+    description.textContent = data[i].description;
+    amount.textContent = data[i].amount;
+    row?.appendChild(category);
+    row?.appendChild(description);
+    row?.appendChild(amount);
   }
 }
 
-async function formSubmit(e: Event, url: string) {
-e.preventDefault();
-const formData = new FormData(form)
-try {
-  const response = await fetch(url, {
-    method: "POST", 
-    body: formData})
-    const data = await response.json();
-    console.log(data)
-} catch (error) {
-  (console.error("Error: ", error))
-}
-}
+formEl.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const formData = new FormData(formEl);
+  const data = Object.fromEntries(formData);
+  fetch(postEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+});
+
 renderData(getEndpoint);
